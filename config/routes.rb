a69c5713613d92223311f8f7ff3fa0bac225c4fc
Quinterious Hall <quinth10@gmail.com>
2020-08-31 18:36:50 +0000

    Added profiles controller, new view, and link on homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
     root to: 'pages#home'
     devise_for :users, controllers: { registrations: 'users/registrations' }
     resources :users do
-        resources :profile
+        resources :profiles
     end
     get 'about', to: 'pages#about'
     resources :contacts, only: :create
